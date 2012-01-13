ea851be305369f60b8a56f4ef6a0908150691d50
Craig Blaszczyk <masterjakul@gmail.com>
2012-01-13 17:46:19 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djangotransifex',
-    version='0.0.1-dev',
+    version='0.0.2',
     description='A django api to transifex',
     long_description=open('README.rst').read(),
     author='Craig Blaszczyk',
