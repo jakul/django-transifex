394b3b5939c58c5962760a05c9a1ff393764e686
Craig Blaszczyk <masterjakul@gmail.com>
2015-01-05 14:29:14 +0000

    use pytest for tests

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,2 @@
-export PYTHONPATH='.'
-
-if [ "$#" -gt "0" ]
-  then
-    python test_project/manage.py test djangotransifex.$@
-  else
-    python test_project/manage.py test djangotransifex
-fi
+#!/bin/bash
+py.test
